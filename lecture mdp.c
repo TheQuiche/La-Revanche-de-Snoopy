dc4e7724b2ef439c482aa215b57b5f39dafc02aa
amine833 <67288506+amine833@users.noreply.github.com>
2023-11-25 19:53:23 +0100

    Update lecture mdp.c
    Il y'a pas le niveau 3 et 4

--- lecture mdp.c
+++ lecture mdp.c
@@ -1,6 +1,4 @@
-#include "biblio.h"
-
-int lecturemdp()
+void lecturemdp(Snoopy *joueur1,Balle *maBalle)
 {
     char *tab[30];
     char mdp[30];
@@ -11,7 +9,7 @@ int lecturemdp()
     if (fichier == NULL)
     {
         printf("Impossible d'ouvrir le fichier.\n");
-        return 1;
+        exit (1);
     }
     printf("Saisir le mot de passe du niveau auquel vous voulez acceder\n");
     scanf("%s",mdp);
@@ -21,27 +19,54 @@ int lecturemdp()
         nbligne++;
     }
     fclose(fichier);
-   for (int i = 0; i < nbligne; i++) {
-        tab[i][strlen(tab[i]) - 1] = '\0'; // Retirer le caractère de nouvelle ligne
-        if (strcmp(mdp, tab[i]) == 0) {
+    for (int i = 0; i < nbligne; i++)
+    {
+        tab[i][strlen(tab[i]) - 1] = '\0';
+        if (strcmp(mdp, tab[i]) == 0)
+        {
             printf("Mot de passe correct.\n");
-            if (i == 0) {
+            if (i == 0)
+            {
                 printf("Vous allez au niveau 1\n");
-            } else if (i == 1) {
+                while (joueur1->vie > 0)
+                {
+                    plateau(joueur1,maBalle);
+                    afficherInfosJoueur(joueur1);
+                    deplacement(joueur1,maBalle);
+                    joueur1->niveau=1;
+                }
+            }
+            else if (i == 1)
+            {
                 printf("Vous allez au niveau 2\n");
-            } else if (i == 2) {
+                while (joueur1->vie > 0)
+                {
+                    niveau2(joueur1,maBalle);
+                    afficherInfosJoueur(joueur1);
+                    deplacement(joueur1,maBalle);
+                    joueur1->niveau=2;
+
+                }
+            }
+            else if (i == 2)
+            {
                 printf("Vous allez au niveau 3\n");
-            } else if (i == 3) {
+                joueur1->niveau=3;
+            }
+            else if (i == 3)
+            {
                 printf("Vous allez au niveau 4\n");
+                joueur1->niveau=4;
             }
-            for (int j = 0; j < nbligne; j++) {
-                free(tab[j]);
-            }
-                return 0;
 
         }
-        free(tab[i]);
+        else
+        {
+            printf("Mot de passe incorrecte\n");
+        }
     }
-    printf("Mot de passe incorrecte\n");
-    return 1;
+    for (int j = 0; j < nbligne; j++)
+    {
+        free(tab[j]);
+    }
 }
