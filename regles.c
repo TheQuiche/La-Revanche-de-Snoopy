09f4e6eebc797c69dbdcd48cbf8127b30a79a81b
amine833 <67288506+amine833@users.noreply.github.com>
2023-11-25 19:53:58 +0100

    regles.c

--- regles.c
+++ regles.c
@@ -1,18 +1,15 @@
-#include "biblio.h"
-
 void regles()
 {
     FILE*fichier=NULL;
-         char ligne[1024];
+    char ligne[1024];
 
     fichier=fopen("regles.txt","r");
     if (fichier == NULL)
     {
         printf("Impossible d'ouvrir le fichier.\n");
-        return 1;
     }
 
-    while (fgets(ligne, sizeof(ligne), fichier) != NULL) {
+    while (fgets(ligne, sizeof(ligne), fichier) != NULL)
+    {
         printf("%s", ligne);
     }
-}
